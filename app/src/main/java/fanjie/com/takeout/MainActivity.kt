7ff711f5bc811dc6ecd8f1c4d3e36e9625659235
wangshaofeng <609895119@qq.com>
2019-07-23 14:11:34 +0800

    '提交项目'

---
+++ app/src/main/java/fanjie/com/takeout/MainActivity.kt
@@ -0,0 +1,12 @@
+package fanjie.com.takeout
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
